b314a7a123f02b36ac8675c3e3b52e1b34795f12
Michael J Cooper <ballermjc@gmail.com>
2018-07-28 15:05:03 -0600

    Angular app created

---
+++ front-end/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'A Front End For Our Projects';
+}

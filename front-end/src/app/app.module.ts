dbd93563b91a40edf162f5d78246c8d11ad30c51
Michael J Cooper <ballermjc@gmail.com>
2018-08-01 20:13:24 -0600

    project now uses less instead of css, components generated

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { NavLinksComponent } from './nav-links/nav-links.component';
+import { AuthLinkComponent } from './auth-link/auth-link.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    NavLinksComponent,
+    AuthLinkComponent,
+    TopNavComponent,
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/auth-link/auth-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-link',
+  templateUrl: './auth-link.component.html',
+  styleUrls: ['./auth-link.component.less']
+})
+export class AuthLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

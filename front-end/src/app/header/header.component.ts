10428b756524dbf1240ce1b093187244987f5c61
Brett Hays <brett.g.hays.coding@gmail.com>
2018-08-15 22:17:48 -0600

    header is dynamic!

--- front-end/src/app/header/header.component.ts
+++ front-end/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
@@ -8,20 +8,28 @@ import { Location } from '@angular/common';
   styleUrls: ['./header.component.less']
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
   header = 'Support ';
 
   title = '';
 
-  constructor(private router: Router, location: Location) { }
+  constructor(private ar: ActivatedRoute, router: Router, location: Location) { }
 
   ngOnInit() {
-    const currentUrl = location.pathname;
-    const tag = currentUrl.slice(1);
-    this.title = tag;
-    console.log('Current URL is: ', currentUrl);
-    console.log('tag is: ', tag);
-    console.log('this.location is :', location.pathname);
+    const currentUrl = location.pathname.slice(1);
+    // const tag = currentUrl.slice(1);
+    this.title = currentUrl;
+    // console.log('Current URL is: ', currentUrl);
+    // console.log('tag is: ', tag);
+    // console.log('this.location is :', location.pathname);
+    // console.log('ar is: ', this.ar.snapshot);
+  }
+
+  ngDoCheck() {
+    // console.log(location.pathname);
+    if (this.title !== location.pathname.slice(1)) {
+      this.title = location.pathname.slice(1);
+    }
   }
 
 }
